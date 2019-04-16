fdbe6c55d2c479ce62631ca481af046eafa53599
zhangpeng <gustzhang@yahoo.com>
2019-04-16 14:22:29 +0800

    update nanovg

--- binary/win32/test.bat
+++ binary/win32/test.bat
@@ -1,5 +1,5 @@
 rem mini_jvm.exe -cp ../lib/minijvm_rt.jar;../lib/minijvm_test.jar test/Foo1 888
-mini_jvm.exe -cp ../lib/minijvm_rt.jar;../lib/glfw_gui.jar test/GuiTest
+mini_jvm.exe -cp ../lib/minijvm_rt.jar;../lib/glfw_gui.jar test/AppManagerTest
 
 @echo compile BpDeepTest.java
 mini_jvm -cp ../lib/minijvm_rt.jar;../lib/janino.jar;../lib/commons-compiler.jar org.codehaus.janino.Compiler  ../res/BpDeepTest.java
